16336dacd05e53303fb8d0e20ba38882d700b3f2
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-10-13 17:02:11 +0330

    update: add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,13 @@ export default defineConfig({
     watch: {
       // In cases where hot reload doesn't work
       usePolling: true
+    }
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@types': path.resolve(__dirname, './src/types')
     }
   }
 });
