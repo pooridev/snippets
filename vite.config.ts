9dc5f93f2107d11e7db379822dc0a0150837e69d
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-10-13 15:37:47 +0330

    update: change dev server config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    origin: 'http://localhost',
+    port: 3000,
+    watch: {
+      // In cases where hot reload doesn't work
+      usePolling: true
+    }
+  }
 });
