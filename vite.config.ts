57ec6b746cb590287fc2f1ef4b0a39bd85917d6c
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-11-10 14:44:51 +0330

    feat: add renderWithProviders helper

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       '@store': path.resolve(__dirname, './src/store'),
       '@types': path.resolve(__dirname, './src/types'),
       '@utils': path.resolve(__dirname, './src/utils'),
+      '@test': path.resolve(__dirname, './src/test'),
     },
   },
 })
